8200db58414959118b9f606564ce22f7d97f5190
Alvaro Francisco Arriagada Ortega <alvaro.malkuth@hotmail.com>
2014-06-08 12:20:46 -0400

    Agregadas todas las imagenes de los productos de la base de datos

---
+++ Codigo/Producto.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Producto.ui'
+#
+# Created: Thu Jun  5 10:40:34 2014
+#      by: pyside-uic 0.2.13 running on PySide 1.1.2
+#
+# WARNING! All changes made in this file will be lost!
+
